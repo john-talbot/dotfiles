cbdee390f1ef0336c5cb6533d4e97545b197be58
John Talbot <john.talbot@tri.global>
2024-07-17 16:56:34 -0400

    Seems to be working well with docker builds now

---
+++ scripts/neovim_install.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TEMP_DIR="$HOME/neovim_temp"
+
+# Download and unpack latest stow tarball
+# This will all happen in TEMP_DIR for easy cleanup
+mkdir -p $TEMP_DIR && cd $TEMP_DIR
+
+# For now we need the prerelease version (Neovim 0.11.0), we'll change this
+# back to `latest` once this version is stabilized
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz >> $LOGFILE
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz >> $LOGFILE
+sudo rm -rf /opt/nvim-linux64 >> $LOGFILE
+sudo tar -C /opt -xf nvim-linux64.tar.gz >> $LOGFILE
+
+rm -rf $TEMP_DIR
---
+++ scripts/treesitter_install.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+TEMP_DIR="$HOME/treesitter_temp"
+
+# Download and unpack latest stow tarball
+# This will all happen in TEMP_DIR for easy cleanup
+mkdir -p $TEMP_DIR && cd $TEMP_DIR
+
+curl -LO https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz >> $LOGFILE
+gunzip tree-sitter-linux-x64.gz
+mv tree-sitter-linux-x64 $HOME/.local/bin/tree-sitter && chmod +x $HOME/.local/bin/tree-sitter
+
+rm -rf $TEMP_DIR
+
