86151183e8a79b591f4c2bb91cab41d752b61dd9
John Talbot <john.talbot@tri.global>
2024-07-18 15:23:52 -0400

    Add docker detection

--- dot-p10k.zsh
+++ dot-p10k.zsh
@@ -12,12 +12,21 @@
 #
 #   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 
+
+# Custom docker segment
+function prompt_my_docker() {
+  if [[ -f /.dockerenv ]]; then
+    p10k segment -t "docker" -f 14 -b 236
+  fi
+}
+
 # Custom Neovim segment for powerlevel10k
 function prompt_my_neovim() {
     if [[ -n "$NVIM" ]]; then
-        p10k segment -t "nvim" -f 46 -b 236
+        p10k segment -t "nvim" -f 2 -b 236
     fi
 }
+
 
 # Temporarily change options.
 'builtin' 'local' '-a' 'p10k_config_opts'
@@ -59,6 +68,7 @@ function prompt_my_neovim() {
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    my_docker
     my_neovim
     anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
