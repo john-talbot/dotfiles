93a830901719399b11e9d78d141023ffbadaf37a
John Talbot <john.andrew.talbot@gmail.com>
2024-07-21 15:57:14 -0400

    Fix paths in autoinstall linux

--- scripts/automated_scripts/autoinstall_apt_packages.bash
+++ scripts/automated_scripts/autoinstall_apt_packages.bash
@@ -8,7 +8,7 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime > /etc/timezone
 # Update and install packages
 apt-get update >> $LOGFILE
 apt-get upgrade -y >> $LOGFILE
-cat "$SCRIPT_DIR/apt-package-list.txt" | xargs apt-get install -y >> $LOGFILE
+cat "$AUTOSCRIPT_DIR/apt-package-list.txt" | xargs apt-get install -y >> $LOGFILE
 
 # Set locale
 locale-gen en_US.UTF-8
