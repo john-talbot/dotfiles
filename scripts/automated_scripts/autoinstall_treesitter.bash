2b0eef126a4e0afe0257a373ee6bb3143ae23d17
John Talbot <john.andrew.talbot@gmail.com>
2024-07-23 21:05:28 -0400

    Testing installation scripts on raspi and seems to be working

--- scripts/automated_scripts/autoinstall_treesitter.bash
+++ scripts/automated_scripts/autoinstall_treesitter.bash
@@ -4,15 +4,20 @@ set -e
 
 TEMP_DIR="$HOME/treesitter_temp"
 
-echo "Installing treesitter" | tee -a $LOGFILE
+echo -n "Installing treesitter... " | tee -a $LOGFILE
+
+ARCH="linux-x64"
+if grep -q Raspberry /proc/cpuinfo; then
+    ARCH="linux-arm64"
+fi
 
 # Download and unpack latest stow tarball
 # This will all happen in TEMP_DIR for easy cleanup
 mkdir -p $TEMP_DIR && cd $TEMP_DIR
 
-curl -LO https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz >> $LOGFILE
-gunzip tree-sitter-linux-x64.gz
-mv tree-sitter-linux-x64 $HOME/.local/bin/tree-sitter && chmod +x $HOME/.local/bin/tree-sitter
+curl -LO "https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-$ARCH.gz" >> $LOGFILE
+gunzip "tree-sitter-$ARCH.gz"
+mv "tree-sitter-$ARCH" $HOME/.local/bin/tree-sitter && chmod +x $HOME/.local/bin/tree-sitter
 
 rm -rf $TEMP_DIR
 
