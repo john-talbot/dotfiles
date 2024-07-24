fd08d767874528945db22990bfb62addaafae81f
John Talbot <john.talbot@tri.global>
2024-07-24 12:11:42 -0400

    Fix bug in oh-my-zsh installation script

--- scripts/automated_scripts/autoinstall_ohmyzsh.bash
+++ scripts/automated_scripts/autoinstall_ohmyzsh.bash
@@ -4,8 +4,8 @@ set -e
 
 # Silently fetch and run install script without any configuration steps
 if [[ -e $HOME/.oh-my-zsh/oh-my-zsh.sh ]]; then
-	echo "Oh-My-Zsh already installed... Skipping." | tee -a $LOGFILE
-    finish
+	echo "Oh-My-Zsh already installed... Skipping!" | tee -a $LOGFILE
+    exit 0
 fi
 
 echo -n "Installing Oh-My-Zsh... " | tee -a $LOGFILE
